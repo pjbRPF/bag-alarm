8dfce7d09a2707debfd8d5f8ca412ea44cbbe8a1
Pete Bell <104009652+pjbRPF@users.noreply.github.com>
2023-04-26 14:08:02 +0100

    Added PIN function

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
+on = 0
+
+def on_gesture_shake():
+    global on
+    on = 1
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
 def on_forever():
-    pass
+    if on == 1:
+        basic.show_icon(IconNames.NO)
+        music.play_sound_effect(music.create_sound_effect(WaveShape.TRIANGLE,
+                5000,
+                228,
+                255,
+                255,
+                2000,
+                SoundExpressionEffect.VIBRATO,
+                InterpolationCurve.LOGARITHMIC),
+            SoundExpressionPlayMode.UNTIL_DONE)
 basic.forever(on_forever)
+
+def on_forever2():
+    global on
+    if input.logo_is_pressed():
+        music.stop_all_sounds()
+        on = 0
+        basic.show_icon(IconNames.YES)
+        basic.pause(5000)
+        basic.clear_screen()
+basic.forever(on_forever2)
--- main.ts
+++ main.ts
@@ -1,19 +1,53 @@
-let on = 0
 input.onGesture(Gesture.Shake, function () {
-    on = 1
+    basic.clearScreen()
+    pin = []
+    alarm = 1
 })
-basic.forever(function () {
-    if (on == 1) {
-        basic.showIcon(IconNames.No)
-        music.playSoundEffect(music.createSoundEffect(WaveShape.Triangle, 5000, 228, 255, 255, 2000, SoundExpressionEffect.Vibrato, InterpolationCurve.Logarithmic), SoundExpressionPlayMode.UntilDone)
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    if (input.buttonIsPressed(Button.A)) {
+        pin.push(1)
+        basic.showString("1")
+        basic.pause(100)
+        basic.clearScreen()
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        pin.push(0)
+        basic.showString("0")
+        basic.pause(100)
+        basic.clearScreen()
+    }
+    if (pin.length == 6) {
+        pinCheck = true
+        for (let index = 0; index <= 5; index++) {
+            if (code[index] != pin[index]) {
+                pinCheck = false
+            }
+        }
+        if (pinCheck == true) {
+            basic.showIcon(IconNames.Yes)
+            music.stopAllSounds()
+            alarm = 0
+        } else {
+            basic.showIcon(IconNames.No)
+            pin = []
+        }
     }
 })
+let pinCheck = false
+let alarm = 0
+let pin: number[] = []
+let code: number[] = []
+code = [
+1,
+1,
+1,
+0,
+0,
+0
+]
+pin = []
 basic.forever(function () {
-    if (input.logoIsPressed()) {
-        music.stopAllSounds()
-        on = 0
-        basic.showIcon(IconNames.Yes)
-        basic.pause(5000)
-        basic.clearScreen()
+    if (alarm == 1) {
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Triangle, 5000, 228, 255, 255, 2000, SoundExpressionEffect.Vibrato, InterpolationCurve.Logarithmic), SoundExpressionPlayMode.InBackground)
     }
 })
